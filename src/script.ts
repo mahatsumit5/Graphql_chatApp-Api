e421a7bfd4737cea448c28c4f2dcb78e19605a2b
mahatsumit5 <mahatsumit5@gmail.com>
2025-04-05 15:43:05 +1100

    Add buildspec and Prisma client setup; update dependencies

---
+++ src/script.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+export async function executeQuery(query: any) {
+  try {
+    return await query;
+  } catch (error) {
+    return {
+      status: false,
+      message: error.message,
+    };
+  } finally {
+    await prisma.$disconnect();
+  }
+}
